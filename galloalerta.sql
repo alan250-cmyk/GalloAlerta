abfc30b4cf3a8a54aa2af16563aefb7a741b94cd
Alan Ibarra <ibarra.alan250@gmail.com>
2026-09-24 19:13:28 -0300

    Actualizar DB

--- galloalerta.sql
+++ galloalerta.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-09-2026 a las 07:37:24
+-- Tiempo de generación: 25-09-2026 a las 00:13:04
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -31,8 +31,8 @@ CREATE TABLE `admins` (
   `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `email` varchar(50) DEFAULT NULL,
-  `usuario` varchar(25) NOT NULL,
-  `contraseña` varchar(50) NOT NULL,
+  `nombreusuario` varchar(25) NOT NULL,
+  `contra` varchar(50) NOT NULL,
   `suspendido` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -40,8 +40,8 @@ CREATE TABLE `admins` (
 -- Volcado de datos para la tabla `admins`
 --
 
-INSERT INTO `admins` (`id`, `nombre`, `email`, `usuario`, `contraseña`, `suspendido`) VALUES
-(1, 'Admin Principal', 'admin@galloalerta.com', 'admin1234', 'admin1234', 0);
+INSERT INTO `admins` (`id`, `nombre`, `email`, `nombreusuario`, `contra`, `suspendido`) VALUES
+(1, 'Admin Principal', 'admin@galloalerta.com', 'admin1234', 'admin12345', 0);
 
 -- --------------------------------------------------------
 
@@ -52,7 +52,7 @@ INSERT INTO `admins` (`id`, `nombre`, `email`, `usuario`, `contraseña`, `suspen
 CREATE TABLE `incidentes` (
   `id` int(11) NOT NULL,
   `asunto` varchar(50) NOT NULL,
-  `descripcion` varchar(100) NOT NULL,
+  `descripcion` varchar(200) DEFAULT NULL,
   `ubicacion` varchar(50) NOT NULL,
   `fecha` datetime NOT NULL DEFAULT current_timestamp(),
   `latitud` float DEFAULT NULL,
@@ -65,9 +65,12 @@ CREATE TABLE `incidentes` (
 --
 
 INSERT INTO `incidentes` (`id`, `asunto`, `descripcion`, `ubicacion`, `fecha`, `latitud`, `longitud`, `resuelto`) VALUES
-(1, 'Se prendió fuego un arbol', 'no sé', 'Arana y Tierra del Fuego', '2026-09-22 20:28:55', NULL, NULL, 1),
-(3, 'Bache pronunciado en la calle', 'Hay un bache en la calle, los autos tienen dificultad para pasar.', 'Eva Perón, Chavarría', '2026-09-22 20:30:39', NULL, NULL, 0),
-(5, 'Cables de alta tension caídos', 'Hay cables de alta tensión caídos en la vereda tras la tormenta con chispas, peligro para los peaton', 'Av. Colón y Catamarca', '2026-09-23 02:11:05', NULL, NULL, 1);
+(1, 'Se prendió fuego un arbol', 'no sé', 'Arana y Tierra del Fuego', '2026-09-22 20:28:55', -34.6532, -58.6185, 1),
+(3, 'Bache pronunciado en la calle', 'Hay un bache en la calle, los autos tienen dificultad para pasar.', 'Eva Perón, Chavarría', '2026-09-22 20:30:39', -34.6534, -58.6198, 0),
+(5, 'Cables de alta tension caídos', 'Hay cables de alta tensión caídos en la vereda tras la tormenta con chispas. Peligro para los peatones.', 'Av. Colón y Catamarca', '2026-09-23 02:11:05', NULL, NULL, 1),
+(12, 'Semáforo intermitente', 'Un semáforo está en mal funcionamiento. Hay peligro de choque en hora pico.', 'Av. Rivadavia y Belgrano', '2026-09-23 19:18:51', -34.6536, -58.6201, 0),
+(21, 'hay un arbol caido', '', 'nicolas davila y puerredon', '2026-09-24 16:38:20', NULL, NULL, 0),
+(24, 'Se rompió una ventana', '', 'Av. San Martín y Belgrano', '2026-09-24 16:45:13', NULL, NULL, 0);
 
 --
 -- Índices para tablas volcadas
@@ -77,7 +80,8 @@ INSERT INTO `incidentes` (`id`, `asunto`, `descripcion`, `ubicacion`, `fecha`, `
 -- Indices de la tabla `admins`
 --
 ALTER TABLE `admins`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `usuario` (`nombreusuario`);
 
 --
 -- Indices de la tabla `incidentes`
@@ -99,7 +103,7 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT de la tabla `incidentes`
 --
 ALTER TABLE `incidentes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
