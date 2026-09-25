85b8f3f5581e6b8b81aacb47a0b2d4588670214e
Alan Ibarra <ibarra.alan250@gmail.com>
2026-09-24 22:48:46 -0300

    Agregar más incidentes de ejemplo a la BD

--- galloalerta.sql
+++ galloalerta.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-09-2026 a las 00:13:04
+-- Tiempo de generación: 25-09-2026 a las 03:48:23
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -55,8 +55,8 @@ CREATE TABLE `incidentes` (
   `descripcion` varchar(200) DEFAULT NULL,
   `ubicacion` varchar(50) NOT NULL,
   `fecha` datetime NOT NULL DEFAULT current_timestamp(),
-  `latitud` float DEFAULT NULL,
-  `longitud` float DEFAULT NULL,
+  `latitud` double DEFAULT NULL,
+  `longitud` double DEFAULT NULL,
   `resuelto` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -65,12 +65,17 @@ CREATE TABLE `incidentes` (
 --
 
 INSERT INTO `incidentes` (`id`, `asunto`, `descripcion`, `ubicacion`, `fecha`, `latitud`, `longitud`, `resuelto`) VALUES
-(1, 'Se prendió fuego un arbol', 'no sé', 'Arana y Tierra del Fuego', '2026-09-22 20:28:55', -34.6532, -58.6185, 1),
-(3, 'Bache pronunciado en la calle', 'Hay un bache en la calle, los autos tienen dificultad para pasar.', 'Eva Perón, Chavarría', '2026-09-22 20:30:39', -34.6534, -58.6198, 0),
-(5, 'Cables de alta tension caídos', 'Hay cables de alta tensión caídos en la vereda tras la tormenta con chispas. Peligro para los peatones.', 'Av. Colón y Catamarca', '2026-09-23 02:11:05', NULL, NULL, 1),
-(12, 'Semáforo intermitente', 'Un semáforo está en mal funcionamiento. Hay peligro de choque en hora pico.', 'Av. Rivadavia y Belgrano', '2026-09-23 19:18:51', -34.6536, -58.6201, 0),
-(21, 'hay un arbol caido', '', 'nicolas davila y puerredon', '2026-09-24 16:38:20', NULL, NULL, 0),
-(24, 'Se rompió una ventana', '', 'Av. San Martín y Belgrano', '2026-09-24 16:45:13', NULL, NULL, 0);
+(1, 'Se prendió fuego un arbol', NULL, 'Arana y Tierra del Fuego', '2026-09-22 20:28:55', -34.68701, -58.638295, 1),
+(3, 'Bache pronunciado en la calle', 'Hay un bache en la calle, los autos tienen dificultad para pasar.', 'Eva Perón, Chavarría', '2026-09-22 20:30:39', NULL, NULL, 1),
+(5, 'Cables de alta tension caídos', 'Hay cables de alta tensión caídos en la vereda tras la tormenta con chispas. Peligro para los peatones.', 'Av. Colón y Catamarca', '2026-09-23 02:11:05', -34.7076, -58.427523, 1),
+(12, 'Semáforo intermitente', 'Un semáforo está en mal funcionamiento. Hay peligro de choque en hora pico.', 'Av. Rivadavia y Belgrano', '2026-09-23 19:18:51', -34.649186, -58.62109, 1),
+(21, 'hay un arbol caido', NULL, 'Int. Agüero y Lanús', '2026-09-24 16:38:20', -34.666836, -58.615692, 0),
+(24, 'Se rompió una ventana', NULL, 'Av. San Martín y Belgrano', '2026-09-24 16:45:13', -34.577909, -58.537629, 0),
+(25, 'Corte de luz', NULL, 'Almirante Brown y Uruguay', '2026-09-24 19:37:47', -34.650588, -58.617346, 1),
+(26, 'Luminaria rota sobre la vereda', NULL, 'Juan José Paso & Julián Pérez', '2026-09-24 21:47:23', -34.651623, -58.607171, 0),
+(27, 'Prenden fuego ramas', 'Están prendiendo fuego unas ramas, los cables de alumbrado generan chispazos.', 'Clinica Modelo de Morón', '2026-09-24 22:36:48', -34.6507461, -58.6170846, 1),
+(28, 'Cables electricos al aire', NULL, 'Almirante Brown y San Martín', '2026-09-24 22:41:23', -34.650407, -58.622688, 0),
+(29, 'Se quemó una luminaria', NULL, '9 de Julio y Santa Fé', '2026-09-24 22:46:09', -34.655827, -58.618595, 0);
 
 --
 -- Índices para tablas volcadas
@@ -103,7 +108,7 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT de la tabla `incidentes`
 --
 ALTER TABLE `incidentes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
